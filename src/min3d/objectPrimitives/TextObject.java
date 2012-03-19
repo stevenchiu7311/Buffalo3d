4752a05fff0ddd0addce37228c398977c4263a14
Steven Chiu <sten7311@hotmail.com>
2012-03-19 16:10:12 +0800

    [Engine3D] Support more common used feature for TextObject.
    Why: Support more common used feature for TextObject.
    How: Paste TextView snapshot to rectangle.
    Verify steps: N/A
    Possible effects: None.

--- src/min3d/objectPrimitives/TextObject.java
+++ src/min3d/objectPrimitives/TextObject.java
@@ -4,11 +4,16 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
+import android.content.res.ColorStateList;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
+import android.graphics.Typeface;
+import android.text.TextUtils;
 import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.TextView.BufferType;
 import min3d.Shared;
 import min3d.Utils;
 import min3d.core.Object3dContainer;
@@ -30,17 +35,7 @@ public class TextObject extends Object3dContainer {
     private float mWidth;
     private float mHeight;
     private float mDepth;
-    private boolean mWrapContent = false;
-    private float mStringWidth;
-    private float mStringHeight;
-    private int mFontSize = 14;
-    private Color4 mFontColor = new Color4(0, 0, 0, 255);
-    private Color4 mBackgroundColor = new Color4(0, 0, 0, 0);
-    private int mTextBitmapWidth;
-    private int mTextBitmapHeight;
-    private Shadow mShadow = new Shadow();
-    private Alignment mAlignment = Alignment.ALIGN_CENTER;
-    private boolean mEllipsize = true;
+    TextView mTextView;
 
     public TextObject(float width, float height, float depth,
             Color4[] sixColor4s, Boolean useUvs, Boolean useNormals,
@@ -50,80 +45,83 @@ public class TextObject extends Object3dContainer {
         mWidth = width;
         mHeight = height;
         mDepth = depth;
+        mTextView = new TextView(Shared.context());
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams((int)(mWidth*MAPPING_PIXEL), (int)(mHeight*MAPPING_PIXEL));
+        mTextView.setLayoutParams(layoutParams);
+    }
 
-        if (sixColor4s != null) {
-            mColors = sixColor4s;
+    public TextObject(float width, float height, float depth,
+            Color4[] sixColor4s) {
+        this(width, height, depth, sixColor4s, true, true, true);
+    }
+
+    public TextObject(float width, float height, float depth,
+            Color4 color) {
+        this(width, height, depth, new Color4[] { color, color, color,
+                color, color, color }, true, true, true);
+    }
+
+    public TextObject(float width, float height, float depth) {
+        this(width, height, depth, null, true, true, false);
+    }
+
+    public void setText(CharSequence text) {
+        mTextView.setText(text);
+        int w = (int)(mWidth*MAPPING_PIXEL);
+        if (w % 2 == 1) {
+            w--;
+        }
+        int h = (int)(mHeight*MAPPING_PIXEL);
+        if (h % 2 == 1) {
+            h--;
+        }
+        Bitmap bitmap = loadBitmapFromView(mTextView, w, h);
+        if (Shared.textureManager().contains(toString())) {
+            Shared.textureManager().deleteTexture(toString());
+        }
+        Shared.textureManager().addTextureId(bitmap, toString(), false);
+        TextureVo textureText = new TextureVo(toString());
+        textureText.repeatU = false;
+        textureText.repeatV = false;
+        textures().removeAll();
+        textures().add(textureText);
+        bitmap.recycle();
+        createVertices();
+    }
+
+    private void createVertices() {
+        float w = mWidth / 2;
+        float h = mHeight / 2;
+        float d = mDepth / 2;
+        short ul, ur, lr, ll;
+        float uvX=1.0f, uvY=1.0f;
+
+        if (vertices().size() == 0) {
+            ul = vertices().addVertex(-w, +h, d, 0f, 0f, 0, 0, 1,
+                    (short)0, (short)0, (short)0, (short)255);
+            ur = vertices().addVertex(+w, +h, d, uvX, 0f, 0, 0, 1,
+                    (short)0, (short)0, (short)0, (short)255);
+            lr = vertices().addVertex(+w, -h, d, uvX, uvY, 0, 0, 1,
+                    (short)0, (short)0, (short)0, (short)255);
+            ll = vertices().addVertex(-w, -h, d, 0f, uvY, 0, 0, 1,
+                    (short)0, (short)0, (short)0, (short)255);
+            Utils.addQuad(this, ul, ur, lr, ll);
         } else {
-            mColors = new Color4[6];
-            mColors[0] = new Color4(255, 0, 0, 255);
-            mColors[1] = new Color4(0, 255, 0, 255);
-            mColors[2] = new Color4(0, 0, 255, 255);
-            mColors[3] = new Color4(255, 255, 0, 255);
-            mColors[4] = new Color4(0, 255, 255, 255);
-            mColors[5] = new Color4(255, 0, 255, 255);
+            vertices().overwriteVerts(
+                    new float[] { -w, +h, d, +w, +h, d, +w, -h, d, -w, -h, d });
+            vertices().setUv(0, 0f, 0f);
+            vertices().setUv(1, uvX, 0f);
+            vertices().setUv(2, uvX, uvY);
+            vertices().setUv(3, 0f, uvY);
         }
     }
 
-    public void setText(String text) {
-
-        Paint textPaint = new Paint();
-        textPaint.setTextSize(mFontSize);
-        textPaint.setAntiAlias(true);
-        textPaint.setShadowLayer(mShadow.getRadius(), mShadow.getHorizontalOffset(), mShadow.getVerticalOffset(), mShadow.getShadowColor());
-        textPaint.setARGB(mFontColor.a, mFontColor.r, mFontColor.g,
-                mFontColor.b);
-        mStringHeight = textPaint.descent() - textPaint.ascent();
-        mStringWidth = textPaint.measureText(text);
-        if (mEllipsize && !mWrapContent) {
-            if (mStringWidth > MAPPING_PIXEL*this.mWidth) {
-                while (textPaint.measureText(text.substring(0,text.length()-2) + "..") > (MAPPING_PIXEL*this.mWidth)) {
-                    text = text.substring(0,text.length()-2);
-                 }
-                text = text.substring(0, text.length()-2) + "..";
-                mStringWidth =  textPaint.measureText(text);
-            }
-        }
-        if (mWrapContent) {
-            this.mHeight = mStringHeight / MAPPING_PIXEL;
-            this.mWidth = mStringWidth / MAPPING_PIXEL;
-        }
-        float tempTextBitmapWidth = mWidth * MAPPING_PIXEL;
-        float tempTextBitmapHeight = mHeight * MAPPING_PIXEL;
-        mTextBitmapWidth = ((tempTextBitmapWidth) - (int) (tempTextBitmapWidth)) > 0 ? (int) (tempTextBitmapWidth) + 1
-                : (int) (tempTextBitmapWidth);
-        mTextBitmapHeight = ((tempTextBitmapHeight) - (int) (tempTextBitmapHeight)) > 0 ? (int) (tempTextBitmapHeight) + 1
-                : (int) (tempTextBitmapHeight);
-        if (mTextBitmapWidth % 2 == 1) {
-            mTextBitmapWidth += 1;
-        }
-        if (mTextBitmapHeight % 2 == 1) {
-            mTextBitmapHeight += 1;
-        }
-
-        Bitmap bitmap = Bitmap.createBitmap(mTextBitmapWidth, mTextBitmapHeight, Bitmap.Config.ARGB_4444);;
-        // get a canvas to paint over the bitmap
-        Canvas canvas = new Canvas(bitmap);
-        bitmap.eraseColor(Color.argb(mBackgroundColor.a, mBackgroundColor.r,
-                mBackgroundColor.g, mBackgroundColor.b));
-        float shift = 0;
-        if (!mWrapContent) {
-            if ((mAlignment == Alignment.ALIGN_CENTER) && (mTextBitmapWidth > mStringWidth)) {
-                shift = ((mTextBitmapWidth - mStringWidth)/2.0f);
-            }
-        }
-        canvas.drawText(text,shift, mStringHeight, textPaint);
-        if (Shared.textureManager().contains(this.toString())) {
-            Shared.textureManager().deleteTexture(this.toString());
-        }
-        Shared.textureManager().addTextureId(bitmap, this.toString(), false);
-        TextureVo textureText = new TextureVo(this.toString());
-        textureText.repeatU = false;
-        textureText.repeatV = false;
-        this.textures().removeAll();
-        this.textures().add(textureText);
-        //saveBitmap(bitmap, "/data/data/min3d.sampleProject1/test.png", "test");
-        bitmap.recycle();
-        createVertices();
+    private Bitmap loadBitmapFromView(View v, int w, int h) {
+        Bitmap b = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        Canvas c = new Canvas(b);
+        v.layout(0, 0, w, h);
+        v.draw(c);
+        return b;
     }
 
     // for debug.
@@ -161,139 +159,206 @@ public class TextObject extends Object3dContainer {
         return result;
     }
 
-    public TextObject(float width, float height, float depth,
-            Color4[] sixColor4s) {
-        this(width, height, depth, sixColor4s, true, true, true);
+    /**
+     * Sets the typeface and style in which the text should be displayed,
+     * and turns on the fake bold and italic bits in the Paint if the
+     * Typeface that you provided does not have all the bits in the
+     * style that you specified.
+     *
+     */
+    public void setTypeface(Typeface tf, int style) {
+        mTextView.setTypeface(tf, style);
     }
 
-    public TextObject(float width, float height, float depth,
-            Color4 color) {
-        this(width, height, depth, new Color4[] { color, color, color,
-                color, color, color }, true, true, true);
+    /**
+     * Return the text the TextView is displaying. If setText() was called with
+     * an argument of BufferType.SPANNABLE or BufferType.EDITABLE, you can cast
+     * the return value from this method to Spannable or Editable, respectively.
+     *
+     * Note: The content of the return value should not be modified. If you want
+     * a modifiable one, you should make your own copy first.
+     */
+    public CharSequence getText() {
+        return mTextView.getText();
     }
 
-    public TextObject(float width, float height, float depth) {
-        this(width, height, depth, null, true, true, true);
+    /**
+     * Returns the length, in characters, of the text managed by this TextObject
+     */
+    public int length() {
+        return mTextView.length();
     }
 
-    public void setFontSize(int size) {
-        mFontSize = size;
+    /**
+     * @return the height of one standard line in pixels.  Note that markup
+     * within the text can cause individual lines to be taller or shorter
+     * than this height, and the layout may contain additional first-
+     * or last-line padding.
+     */
+    public int getLineHeight() {
+        return mTextView.getLineHeight();
     }
 
-    public int getFontSize() {
-        return mFontSize;
+    /**
+     * @return the size (in pixels) of the default text size in this TextObject.
+     */
+    public float getTextSize() {
+        return mTextView.getTextSize();
     }
 
-    public void setFontColor(Color4 color) {
-        mFontColor = color;
+    /**
+     * Set the default text size to the given value, interpreted as "scaled
+     * pixel" units.  This size is adjusted based on the current density and
+     * user font size preference.
+     *
+     * @param size The scaled pixel size.
+     */
+    public void setTextSize(float size) {
+        mTextView.setTextSize(size);
     }
 
-    public Color4 getFontColor() {
-        return mFontColor;
+    /**
+     * Set the default text size to a given unit and value.
+     *
+     * @param unit The desired dimension unit.
+     * @param size The desired size in the given units.
+     *
+     */
+    public void setTextSize(int unit, float size) {
+        mTextView.setTextSize(unit,size);
     }
 
-    public void setBackgroundColor(Color4 color) {
-        mBackgroundColor = color;
+    /**
+     * Sets the typeface and style in which the text should be displayed.
+     * Note that not all Typeface families actually have bold and italic
+     * variants.
+     *
+     */
+    public void setTypeface(Typeface tf) {
+        mTextView.setTypeface(tf);
     }
 
-    public Color4 getBackgroundColor() {
-        return mBackgroundColor;
+    /**
+     * @return the current typeface and style in which the text is being
+     * displayed.
+     */
+    public Typeface getTypeface() {
+        return mTextView.getTypeface();
     }
 
-    public void destory() {
-        Shared.textureManager().deleteTexture(this.toString());
+    /**
+     * Sets the text color for all the states (normal, selected,
+     * focused) to be this color.
+     *
+     */
+    public void setTextColor(int color) {
+        mTextView.setTextColor(color);
     }
 
-    public void setWrapContent(Boolean wrapContent) {
-        this.mWrapContent = wrapContent;
+    /**
+     * Sets the text color.
+     *
+     */
+    public void setTextColor(ColorStateList colors) {
+        mTextView.setTextColor(colors);
     }
 
-    private void createVertices() {
-        float w = mWidth / 2;
-        float h = mHeight / 2;
-        float d = mDepth / 2;
-        short ul, ur, lr, ll;
-        float uvX, uvY;
-        if (mWrapContent) {
-            uvX = mStringWidth / mTextBitmapWidth;
-            uvY = mStringHeight / mTextBitmapHeight;
-        } else {
-            uvX = (mWidth * 256.0f) / mTextBitmapWidth;
-            uvY = (mHeight * 256.0f) / mTextBitmapHeight;
-        }
-        if (vertices().size() == 0) {
-            ul = this.vertices().addVertex(-w, +h, d, 0f, 0f, 0, 0, 1,
-                    mColors[0].r, mColors[0].g, mColors[0].b, mColors[0].a);
-            ur = this.vertices().addVertex(+w, +h, d, uvX, 0f, 0, 0, 1,
-                    mColors[0].r, mColors[0].g, mColors[0].b, mColors[0].a);
-            lr = this.vertices().addVertex(+w, -h, d, uvX, uvY, 0, 0, 1,
-                    mColors[0].r, mColors[0].g, mColors[0].b, mColors[0].a);
-            ll = this.vertices().addVertex(-w, -h, d, 0f, uvY, 0, 0, 1,
-                    mColors[0].r, mColors[0].g, mColors[0].b, mColors[0].a);
-            Utils.addQuad(this, ul, ur, lr, ll);
-        } else {
-            this.vertices().overwriteVerts(
-                    new float[] { -w, +h, d, +w, +h, d, +w, -h, d, -w, -h, d });
-            this.vertices().setUv(0, 0f, 0f);
-            this.vertices().setUv(1, uvX, 0f);
-            this.vertices().setUv(2, uvX, uvY);
-            this.vertices().setUv(3, 0f, uvY);
-        }
+    /**
+     * Return the set of text colors.
+     *
+     * @return Returns the set of text colors.
+     */
+    public final ColorStateList getTextColors() {
+        return mTextView.getTextColors();
     }
 
-    public enum Alignment {
-        ALIGN_LEFT,
-        ALIGN_CENTER,
+    /**
+     * Return the current color selected for normal text.
+     *
+     * @return Returns the current text color.
+     */
+    public final int getCurrentTextColor() {
+        return mTextView.getCurrentTextColor();
     }
 
-    public void setAlignment(Alignment alignment) {
-        mAlignment = alignment;
+    /**
+     * Gives the text a shadow of the specified radius and color, the specified
+     * distance from its normal position.
+     *
+     */
+    public void setShadowLayer(float radius, float dx, float dy, int color) {
+        mTextView.setShadowLayer(radius, dx, dy, color);
     }
 
-    public void setEllipsize(boolean ellipsize) {
-        this.mEllipsize = ellipsize;
+    /**
+     * Sets the horizontal alignment of the text and the
+     * vertical gravity that will be used when there is extra space
+     * in the TextObject beyond what is required for the text itself.
+     *
+     * @see android.view.Gravity
+     */
+    public void setGravity(int gravity) {
+        mTextView.setGravity(gravity);
     }
 
-    public void setShadow(float radius, float dx, float dy, int color) {
-        mShadow.setShadow(radius, dx, dy, color);
+    /**
+     * Returns the horizontal and vertical alignment of this TextObject.
+     *
+     * @see android.view.Gravity
+     */
+    public int getGravity() {
+        return mTextView.getGravity();
     }
 
-    class Shadow {
-        float mRadius = 0.5f;
-        float mHorizontalOffset = 1.0f;
-        float mVerticalOffset = 1.0f;
-        int mColor = Color.WHITE;
-        public void shadow(float radius, float dx, float dy, int color) {
-            mRadius = radius;
-            mHorizontalOffset = dx;
-            mVerticalOffset = dy;
-            mColor = color;
-        }
+    /**
+     * Sets the text that this TextObject is to display (see
+     * {@link #setText(CharSequence)}) and also sets whether it is stored
+     * in a styleable/spannable buffer and whether it is editable.
+     *
+     */
+    public void setText(CharSequence text, BufferType type) {
+        mTextView.setText(text, type);
+    }
 
-        public void shadow() {
-        }
+    public final void setText(char[] text, int start, int len) {
+        mTextView.setText(text, start, len);
+    }
 
-        public void setShadow(float radius, float dx, float dy, int color) {
-            mRadius = radius;
-            mHorizontalOffset = dx;
-            mVerticalOffset = dy;
-            mColor = color;
-        }
+    /**
+     * Return the number of lines of text, or 0 if the internal Layout has not
+     * been built.
+     */
+    public int getLineCount() {
+        return mTextView.getLineCount();
+    }
 
-        public float getRadius() {
-            return mRadius;
-        }
+    /**
+     * Sets the properties of this field (lines, horizontally scrolling,
+     * transformation method) to be for a single-line input.
+     *
+     * @attr ref android.R.styleable#TextView_singleLine
+     */
+    public void setSingleLine() {
+        mTextView.setSingleLine();
+    }
 
-        public float getHorizontalOffset() {
-            return mHorizontalOffset;
-        }
+    /**
+     * Causes words in the text that are longer than the view is wide
+     * to be ellipsized instead of broken in the middle.  You may also
+     * want to {@link #setSingleLine} or {@link #setHorizontallyScrolling}
+     * to constrain the text to a single line.  Use <code>null</code>
+     * to turn off ellipsizing.
+     *
+     */
+    public void setEllipsize(TextUtils.TruncateAt where) {
+        mTextView.setEllipsize(where);
+    }
 
-        public float getVerticalOffset() {
-            return mVerticalOffset;
-        }
-
-        public int getShadowColor() {
-            return mColor;
-        }
+    /**
+     * Returns where, if anywhere, words that are longer than the view
+     * is wide should be ellipsized.
+     */
+    public TextUtils.TruncateAt getEllipsize() {
+        return mTextView.getEllipsize();
     }
 }
