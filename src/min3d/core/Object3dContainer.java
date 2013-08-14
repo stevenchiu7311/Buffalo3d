d17b35a59673ba9e5758ca344b506d29bb2c1f4a
Steven Chiu <sten7311@hotmail.com>
2013-08-14 17:30:28 +0800

    [Engine3D] Make up object's parent pointer clean in removeAllChildren
    Why: Make up object's parent pointer clean in removeAllChildren.
    How: Make up object's parent pointer clean in removeAllChildren.
    Verify steps: N/A
    Possible effects: none

--- src/min3d/core/Object3dContainer.java
+++ src/min3d/core/Object3dContainer.java
@@ -168,6 +168,10 @@ public class Object3dContainer extends Object3d implements IObject3dContainer, I
      * {@inheritDoc}
      */
     public void removeAllChildren() {
+        for (Object3d o : mChildren) {
+            o.parent(null);
+            o.scene(null);
+        }
         mChildren.clear();
     }
 
