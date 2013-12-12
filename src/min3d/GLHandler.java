5a5222d3ac217717bd9756c1f6a64c2e4ec421b1
Steven Chiu <sten7311@hotmail.com>
2013-12-12 11:15:53 +0800

    [Engine3D] Append the missed file
    Why: Append the missed file.
    How: Append GLHandler and IRendererGLSurfaceViewConfig.
    Verify steps: N/A
    Possible effects: none

---
+++ src/min3d/GLHandler.java
@@ -0,0 +1,78 @@
+package min3d;
+
+import android.opengl.GLSurfaceView;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.util.Log;
+
+public class GLHandler extends Handler {
+    private final static String TAG = "GLHandler";
+    private final static boolean DEBUG = false;
+
+    GLSurfaceView mProxy;
+    long mLastTime;
+    GLHandlerCallback mGLHandlerCallback;
+
+    public GLHandler(Looper looper, GLSurfaceView proxy) {
+        super(looper);
+        mProxy = proxy;
+    }
+
+    /**
+     * Subclasses must implement this to receive messages.
+     */
+    public void handleMessage(Message msg) {
+        if (DEBUG) {
+            Log.i(TAG,"GLHandler handle message period:"+(System.currentTimeMillis() - mLastTime));
+        }
+        mLastTime = System.currentTimeMillis();
+    }
+
+    /**
+     * Handle system messages here.
+     */
+    public void dispatchMessage(Message msg) {
+        if (mGLHandlerCallback != null) {
+            mGLHandlerCallback.onDispatchMessage(msg);
+        }
+        Runnable callback = msg.getCallback();
+        if (callback != null) {
+            if (mProxy != null) {
+                mProxy.queueEvent(callback);
+            }
+        } else {
+            Message newMsg = Message.obtain(msg);
+            if (mProxy != null) {
+                mProxy.queueEvent(new OneShotTask(this, newMsg));
+            }
+        }
+    }
+
+    class OneShotTask implements Runnable {
+        GLHandler mHandler;
+        Message mMsg;
+
+        OneShotTask(GLHandler handler, Message msg) {
+            mHandler = handler;
+            mMsg = msg;
+        }
+
+        public void run() {
+            if (mHandler != null) {
+                mHandler.handleMessage(mMsg);
+            }
+            if (mMsg != null) {
+                mMsg.recycle();
+            }
+        }
+    }
+
+    public void setCallback(GLHandlerCallback callback) {
+        mGLHandlerCallback = callback;
+    }
+
+    public interface GLHandlerCallback {
+        void onDispatchMessage(Message msg);
+    }
+}
---
+++ src/min3d/interfaces/IRendererGLSurfaceViewConfig.java
@@ -0,0 +1,10 @@
+package min3d.interfaces;
+
+/**
+ * Any GlSurfaceView settings that needs to be executed before
+ * GLSurfaceView.setRenderer() can be done by overriding this method.
+ * A couple examples are included in comments below.
+ */
+public interface IRendererGLSurfaceViewConfig {
+    void onConfigSetting();
+}
